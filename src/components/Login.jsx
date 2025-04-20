3d0c976ce9007d08f26aff2f40f94ae76064215a
Rashmi Sinha <rashmisinha7@Rashmis-MacBook-Air.local>
2025-04-20 22:58:17 +0530

    added login handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
@@ -12,7 +12,41 @@ const Login = () => {
     console.log(counter);
   }, []);
 
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", res.data.token);
+    } catch (err) {
+      console.error("Login failed", err);
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <form onSubmit={handleLogin}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,6 +9,14 @@ const handlers = [
       price: 9.99,
     });
   }),
+  http.post("*/api/login", () => {
+    return HttpResponse.json({
+      id: "123",
+      token: "asbcdgg",
+      userName: "Rashmi",
+      role: "user",
+    });
+  }),
 ];
 
 export default handlers;
