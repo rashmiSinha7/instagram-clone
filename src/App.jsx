812d404dc9755bfb02892e17ea5a7031417a8a87
Rashmi Sinha <rashmi.sinha1st@gmail.com>
2025-05-07 22:26:08 +0530

    added lazy loading anf errorboundaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-
+import React, { Suspense } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Login from "./components/Login";
-import HomePage from "./components/HomePage";
 
 function App() {
-
+  const Login = React.lazy(() => import("./components/Login"));
+  const HomePage = React.lazy(() => import("./components/HomePage"));
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" index element={<Login/>} />
-          <Route path="/account/:userName" element={<HomePage/>}/>
+          <Route
+            path="/login"
+            index
+            element={
+              <Suspense fallback={<h1>Loading....</h1>}>
+                <Login />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/account/:userName"
+            element={
+              <Suspense fallback={<h1>Loading...</h1>}>
+                <HomePage />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
